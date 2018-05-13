443230f8b1f9b75f928a1ed74737820ca1c7bf0d
srinivasarao kondapaturi <srinivasisid@gmail.com>
2018-05-13 23:25:19 +0530

    first commit

---
+++ InsertDocuments/Controllers/HomeController.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InsertDocuments.Models;
+using MongoDB.Bson;
+using InsertDocuments.Services;
+namespace InsertDocuments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Insert()
+        {
+            var document = new BsonDocument
+            {
+                { "item", "canvas" },
+                { "qty", 100 },
+                { "tags", new BsonArray { "cotton" } },
+                { "size", new BsonDocument { { "h", 28 }, { "w", 35.5 }, { "uom", "cm" } } }
+            };
+
+            MongoDBService mongoDBService = new MongoDBService();
+            mongoDBService.InsertDocument(document);
+        }
+
+        public void InsertMany()
+        {
+            var documents = new BsonDocument[]
+            {
+                new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "qty", 25 },
+                    { "tags", new BsonArray { "blank", "red" } },
+                    { "size", new BsonDocument { { "h", 14 }, { "w", 21 }, {  "uom", "cm"} } }
+                },
+                new BsonDocument
+                {
+                    { "item", "mat" },
+                    { "qty", 85 },
+                    { "tags", new BsonArray { "gray" } },
+                    { "size", new BsonDocument { { "h", 27.9 }, { "w", 35.5 }, {  "uom", "cm"} } }
+                },
+                new BsonDocument
+                {
+                    { "item", "mousepad" },
+                    { "qty", 25 },
+                    { "tags", new BsonArray { "gel", "blue" } },
+                    { "size", new BsonDocument { { "h", 19 }, { "w", 22.85 }, {  "uom", "cm"} } }
+                },
+                new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "qty", 25 },
+                    { "tags", new BsonArray { "blank", "red" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "notebook" },
+                    { "qty", 50 },
+                    { "tags", new BsonArray { "red", "blank" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "paper" },
+                    { "qty", 100 },
+                    { "tags", new BsonArray { "red", "blank", "plain" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "planner" },
+                    { "qty", 75 },
+                    { "tags", new BsonArray { "blank", "red" } },
+                    { "dim_cm", new BsonArray { 22.85, 30 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "postcard" },
+                    { "qty", 45 },
+                    { "tags", new BsonArray { "blue" } },
+                    { "dim_cm", new BsonArray { 10, 15.25 } }
+                }
+            ,
+                new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "qty", 25 },
+                    { "size", new BsonDocument { { "h", 14 }, { "w", 21 }, { "uom", "cm" } } },
+                    { "status", "A" }
+                },
+                new BsonDocument
+                {
+                    { "item", "notebook" },
+                    { "qty", 50 },
+                    { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+                    { "status", "A" }
+                },
+                new BsonDocument
+                {
+                    { "item", "paper" },
+                    { "qty", 100 },
+                    { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+                    { "status", "D" }
+                },
+                new BsonDocument
+                {
+                    { "item", "planner" },
+                    { "qty", 75 },
+                    { "size", new BsonDocument { { "h", 22.85 }, { "w", 30 }, { "uom", "cm" } } },
+                    { "status", "D" }
+                },
+                new BsonDocument
+                {
+                    { "item", "postcard" },
+                    { "qty", 45 },
+                    { "size", new BsonDocument { { "h", 10 }, { "w", 15.25 }, { "uom", "cm" } } },
+                    { "status", "A" } },
+                new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "qty", 25 },
+                    { "tags", new BsonArray { "blank", "red" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "notebook" },
+                    { "qty", 50 },
+                    { "tags", new BsonArray { "red", "blank" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "paper" },
+                    { "qty", 100 },
+                    { "tags", new BsonArray { "red", "blank", "plain" } },
+                    { "dim_cm", new BsonArray { 14, 21 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "planner" },
+                    { "qty", 75 },
+                    { "tags", new BsonArray { "blank", "red" } },
+                    { "dim_cm", new BsonArray { 22.85, 30 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "postcard" },
+                    { "qty", 45 },
+                    { "tags", new BsonArray { "blue" } },
+                    { "dim_cm", new BsonArray { 10, 15.25 } }
+                },
+                new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 5 } },
+                            new BsonDocument { { "warehouse", "C" }, { "qty", 15 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "notebook" },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "C" }, { "qty", 5 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "paper" },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 60 } },
+                            new BsonDocument { { "warehouse", "B" }, { "qty", 15 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "planner" },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 40 } },
+                            new BsonDocument { { "warehouse", "B" }, { "qty", 5 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "postcard" },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "B" }, { "qty", 15 } },
+                            new BsonDocument { { "warehouse", "C" }, { "qty", 35 } } }
+                        }
+                },new BsonDocument
+                {
+                    { "item", "journal" },
+                    { "status", "A" },
+                    { "size", new BsonDocument { { "h", 14 }, { "w", 21 }, { "uom", "cm" } } },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 5 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "notebook" },
+                    { "status", "A" },
+                    { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "C" }, { "qty", 5 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "paper" },
+                    { "status", "D" },
+                    { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 60 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "planner" },
+                    { "status", "D" },
+                    { "size", new BsonDocument { { "h", 22.85 }, { "w", 30 }, { "uom", "cm" } } },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "A" }, { "qty", 40 } } }
+                        }
+                },
+                new BsonDocument
+                {
+                    { "item", "postcard" },
+                    { "status", "A" },
+                    { "size", new BsonDocument { { "h", 10 }, { "w", 15.25 }, { "uom", "cm" } } },
+                    { "instock", new BsonArray
+                        {
+                            new BsonDocument { { "warehouse", "B" }, { "qty", 15 } },
+                            new BsonDocument { { "warehouse", "C" }, { "qty", 35 } } }
+                        }
+                },
+                new BsonDocument { { "_id", 1 }, { "item", BsonNull.Value } },
+                new BsonDocument { { "_id", 2 } }
+            };
+            MongoDBService mongoDBService = new MongoDBService();
+            mongoDBService.InsertManyDocument(documents);
+        }
+
+        public void Update()
+        {
+            MongoDBService mongoDBService = new MongoDBService();
+            mongoDBService.Update();
+        }
+        public IActionResult Index()
+        {
+            MongoDBService mongoDBService = new MongoDBService();
+            var result = mongoDBService.Get();
+            ViewBag.result = result.ToJson();
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        /*
+        db.collection.insertOne()	Inserts a single document into a collection.
+        db.collection.insertMany()	db.collection.insertMany() inserts multiple documents into a collection.
+        db.collection.insert()	db.collection.insert() inserts a single document or multiple documents into a collection.
+
+        db.collection.update() when used with the upsert: true option.
+        db.collection.updateOne() when used with the upsert: true option.
+        db.collection.updateMany() when used with the upsert: true option.
+        db.collection.findAndModify() when used with the upsert: true option.
+        db.collection.findOneAndUpdate() when used with the upsert: true option.
+        db.collection.findOneAndReplace() when used with the upsert: true option.
+        db.collection.save().
+        db.collection.bulkWrite()
+         */
+    }
+}
---
+++ InsertDocuments/Services/MongoDBService.cs
@@ -0,0 +1,129 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsertDocuments.Services
+{
+    public class MongoDBService
+    {
+        private const string ConnectionString = "mongodb://localhost";
+
+        public MongoDBService()
+        {
+            var clinet = new MongoClient(ConnectionString);
+            var database = clinet.GetDatabase("MVCCURD");
+            collection = database.GetCollection<BsonDocument>("Students");
+        }
+
+        public MongoDBService(IMongoCollection<BsonDocument> collection)
+        {
+            collection = collection ?? throw new ArgumentNullException(nameof(collection));
+        }
+
+        public IMongoCollection<BsonDocument> collection { get; }
+
+        public void InsertDocument(BsonDocument document)
+        {
+            collection.InsertOne(document);
+        }
+
+        public void InsertManyDocument(BsonDocument[] documents)
+        {
+            collection.InsertMany(documents);
+        }
+
+        public List<BsonDocument> Get()
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            //var filter = Builders<BsonDocument>.Filter.Eq("item", "canvas");
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "D");
+            //var filter = Builders<BsonDocument>.Filter.In("status", new[] { "A", "D" });
+
+            // AND
+            //var filter = builder.And(builder.Eq("status", "A"), builder.Lt("qty", 30));
+
+            // OR
+            //var filter = builder.Or(builder.Eq("status", "A"), builder.Lt("qty", 30)); 
+
+            // AND & OR
+            //var filter = builder.And(
+            //    builder.Eq("status", "A"),
+            //    builder.Or(builder.Lt("qty", 30), builder.Regex("item", new BsonRegularExpression("^p"))));
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("size", new BsonDocument { { "h", 14 }, { "w", 21 }, { "uom", "cm" } });
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("size.uom", "in");
+
+            //var filter = Builders<BsonDocument>.Filter.Lt("size.h", 15);
+
+            /*
+            
+            var filter = builder.And(builder.Lt("size.h", 15), builder.Eq("size.uom", "in"), builder.Eq("status", "D"));
+            */
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("tags", new[] { "red", "blank" });
+
+            // var filter = Builders<BsonDocument>.Filter.All("tags", new[] { "red", "blank" });
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("tags", new[] { "red", "blank" });
+            //var filter = Builders<BsonDocument>.Filter.All("tags", new[] { "red", "blank" });
+            //var filter = Builders<BsonDocument>.Filter.Eq("tags", "red");
+
+            //var filter = Builders<BsonDocument>.Filter.Gt("dim_cm", 25);
+            //var filter = builder.And(builder.Gt("dim_cm", 15), builder.Lt("dim_cm", 20));
+            //var filter = Builders<BsonDocument>.Filter.ElemMatch<BsonValue>("dim_cm", new BsonDocument { { "$gt", 22 }, { "$lt", 30 } });
+
+            //var filter = Builders<BsonDocument>.Filter.Gt("dim_cm.1", 25);
+            //var filter = Builders<BsonDocument>.Filter.Size("tags", 3);
+            //var filter = Builders<BsonDocument>.Filter.AnyEq("instock", new BsonDocument { { "warehouse", "A" }, { "qty", 5 } });
+            //var filter = Builders<BsonDocument>.Filter.AnyEq("instock", new BsonDocument { { "qty", 5 }, { "warehouse", "A" } });
+            //var filter = Builders<BsonDocument>.Filter.Lte("instock.qty", 20);
+            //var filter = Builders<BsonDocument>.Filter.Lte("instock.0.qty", 20);
+            //var filter = Builders<BsonDocument>.Filter.ElemMatch<BsonValue>("instock", new BsonDocument { { "qty", 5 }, { "warehouse", "A" } });
+            // var filter = Builders<BsonDocument>.Filter.ElemMatch<BsonValue>("instock", new BsonDocument { { "qty", new BsonDocument { { "$gt", 10 }, { "$lte", 20 } } } });
+            // var filter = builder.And(builder.Gt("instock.qty", 10), builder.Lte("instock.qty", 20));
+            //var filter = builder.And(builder.Eq("instock.qty", 5), builder.Eq("instock.warehouse", "A"));
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "A");
+            //var projection = Builders<BsonDocument>.Projection.Include("item").Include("status");
+            //var projection = Builders<BsonDocument>.Projection.Exclude("status").Exclude("instock");
+            //var filter = Builders<BsonDocument>.Filter.Empty;
+            //var result = collection.Find(filter).ToList();
+            //var result = collection.Find<BsonDocument>(filter).Project(projection).ToList();
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "A");
+            //var projection = Builders<BsonDocument>.Projection.Include("item").Include("status").Include("size.uom");
+            //var result = collection.Find<BsonDocument>(filter).Project(projection).ToList();
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "A");
+            //var projection = Builders<BsonDocument>.Projection.Exclude("size.uom");
+            //var result = collection.Find<BsonDocument>(filter).Project(projection).ToList();
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "A");
+            //var projection = Builders<BsonDocument>.Projection.Include("item").Include("status").Include("instock.qty");
+            //var result = collection.Find<BsonDocument>(filter).Project(projection).ToList();
+            //var filter = Builders<BsonDocument>.Filter.Eq("status", "A");
+            //var projection = Builders<BsonDocument>.Projection.Include("item").Include("status").Slice("instock", -1);
+            //var result = collection.Find<BsonDocument>(filter).Project(projection).ToList();
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("item", BsonNull.Value);
+            //var result = collection.Find(filter).ToList();
+            //var filter = Builders<BsonDocument>.Filter.Type("item", BsonType.Null);
+            //var result = collection.Find(filter).ToList();
+
+            var filter = Builders<BsonDocument>.Filter.Exists("item", false);
+            var result = collection.Find(filter).ToList();
+            return result;
+        }
+
+       public void Update()
+        {
+            var filter = Builders<BsonDocument>.Filter.Lt("qty", 50);
+            var update = Builders<BsonDocument>.Update.Set("size.uom", "in").Set("status", "P").CurrentDate("lastModified");
+            var result = collection.UpdateMany(filter, update);
+        }
+    }
+}
